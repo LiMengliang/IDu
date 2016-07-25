c1b3ffa3b943736805520b40680821f6b75170cb
LiMengliang <limengliang4007@126.com>
2016-07-25 20:50:40 +0800

    + Add widget moduel + ZoomableImageView

--- app/src/main/java/com/redoc/idu/view/MainActivity.java
+++ app/src/main/java/com/redoc/idu/view/MainActivity.java
@@ -1,15 +1,25 @@
 package com.redoc.idu.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.redoc.idu.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button)(findViewById(R.id.enterNext));
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 }
